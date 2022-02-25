0a9c3b7d72fad8d933053f0320bc6656f2d0c732
Sylwia Waligóra <sylwia.waligora@ageno.pl>
2022-02-25 01:28:35 +0100

    Add another data for render autocomplete

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+const technologies = [
+    'Java Script', 'React', 'Next.js', 'HTML', 'scss', 'less', 'sass', 'Vue', 'Vanilla', 'Typescript', 'Angular', 'Python', 'Java', 'Go', 'Ruby', 'C++', 'C#'
+];
+
+const names = [
+    'Aneta', 'Paulina', 'Sebastian', 'Kacper', 'Sylwia', 'Julia'
+];
+
+const animals = [
+    'Horse', 'Dog', 'Cat', 'Mouse', 'Bird'
+]
+
+export {
+    technologies,
+    names,
+    animals
+};
--- src/data/technologies.js
+++
@@ -1,5 +0,0 @@
-const technologies = [
-    'Java Script', 'React', 'Next.js', 'HTML', 'scss', 'less', 'sass', 'Vue', 'Vanilla', 'Typescript', 'Angular', 'Python', 'Java', 'Go', 'Ruby', 'C++', 'C#'
-];
-
-export { technologies };
